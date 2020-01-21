7beec25d5328ded9446d22dad7a336d82afeeda0
syunyun <pa324@naver.com>
2020-01-22 03:47:38 +0900

    change csv parsing module to csvtojson

--- nugu/CsvParser.js
+++ nugu/CsvParser.js
@@ -1,30 +1,28 @@
-const parser = require('csv-parser');
-const errorMessage = require('../error/errorLogFormat').format;
-const fs = require('fs');
-
-
-// 재료 csv 파일 경로
-const Ingredient = __dirname + "/data/"+ "Receipe" + ".csv";
-// 레시피 csv 파일 경로
-const Receipe= __dirname + "/data/"+ "Receipe" + ".csv";
-const CookingOrder = __dirname + "/data/"+ "Receipe" + ".csv";
-
-//레시피 저장된 배열
-let  receipes= [];
-
-//재료 저장된 배열
-let ingredients = [];
-
-let orders = [];
-
-
-fs.createReadStream(Ingredient)
-                    .pipe(parser())
-                    .on('data', (data) => {
-                        ingredients.push(data);
-                    })
-                    .on('end', async () => {
-                        console.log(ingredients)
-                    })
-                   
+module.exports = class CsvParser {
+    
+    constructor() {
+        this.parser = require('csvtojson');
+        this.errorMessage = require('../error/errorLogFormat').format;
+        
+    }
   
+    async parseCsv(fileName) {
+
+        
+        const directory = __dirname + "/data/"+ fileName + ".csv";
+        
+        if(typeof(fileName)  !== 'string') {
+            console.log(this.errorMessage.typeError);
+            return;
+        }   
+        
+        const results = await this.parser().fromFile(directory);
+        console.log(results)
+        return results;
+      
+    }
+
+
+    
+
+}
