7977327d75cafbf63fddc945e5a535d7e20dd220
syunyun <pa324@naver.com>
2020-01-23 10:37:05 +0900

    test api code

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,8 @@ router.post('/getReceipe.threeIngredient', async (req,res) => {
     const result = await nuguService(payload);
      console.log(payload.action.parameters);
     //if(result == null) res.status(500).end();
-    const receipe = {receipeName : "김치 볶음밥"};
+    const receipe = {"receipeName" : "김치 볶음밥"};
+    console.log(receipe);
     res.json(receipe);
 })
 
