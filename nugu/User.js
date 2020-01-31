5f6096a0b6c0f9c8df3b03846624d583c4f7d3f8
syunyun <pa324@naver.com>
2020-01-31 09:18:57 +0900

    레시피 찾기 로직 수정

--- nugu/User.js
+++ nugu/User.js
@@ -14,7 +14,7 @@ const db = require('../db/db');
         this.id = null;
         this.ingredients = [];
         this.receipes = null;
-	this.MAX_INGREDIENT_COUNT = 3;
+	    this.MAX_INGREDIENT_COUNT = 3;
         this.MAX_RECEIPE_COUNT = 6; 
 
     }
@@ -41,40 +41,76 @@ const db = require('../db/db');
      
     async filterIngredientCode() {
 
+        
         const ingredientList = await db.getIngredients();
         let filteredByEntity = new Map();
+        
 
-        //filtered by ingredient entity
+        //대표 재료 기준으로 필터링
         this.ingredients.forEach(ingredientName => {
-            ingredientList.forEach(ingredient => {;
-		 if(ingredient["I-title"] === ingredientName) {
-                       
+        
+            
+            let frequency = 1;
+
+
+            //입력받은 재료와, 등록된 재료를 비교해서 대표재료의 빈도수를 구하고 Map 자료형에 저장한다.
+            ingredientList.forEach(ingredient => {
+                   
+                    
+                    const mainIngredient = ingredient["I-title"]
                     const receipeCode = ingredient["R-code"];
                     const receipeCounts = ingredient["I-count"];
-                    filteredByEntity.set(receipeCode,receipeCounts);
-                
-                }
+                    
+                    // 입력받은 재료와 등록된 재료와 같은 경우
+                    if(mainIngredient === ingredientName) {
+                        
+
+                        // 이미 Map에 등록된 경우 빈도수 증가
+                        if(filteredByEntity.has(receipeCode)) {
+                            let beforeFrequency = filteredByEntity.get(receipeCode).frequency;
+                            filteredByEntity.set(receipeCode,{ "receipeCounts" : receipeCounts, "frequency" : ++beforeFrequency});
+                        }
+                        // 처음 찾은 레시피 이면 빈도수 1로 설정
+                        else {
+                            filteredByEntity.set(receipeCode,{ "receipeCounts" : receipeCounts, "frequency" : frequency});
+                        }
+                             
+                    }
+                    
+                    
             })       
         })
 
-        //filtered by ingredient counts
-        let filteredByCounts = []
-	filteredByEntity[Symbol.iterator] = function* () {
-	   yield* [...this.entries()].sort((a,b) => a[1] - b[1]);
-	};
-        for(let [key,value] of filteredByEntity) {
+
+
+        // 빈도수 기준으로 정렬
+        const sortedByFrequency = new Map([...filteredByEntity.entries()].sort((a, b) => b[1].frequency - a[1].frequency));
+        let filteredByFrequency = [];
+
+        for(let [key,value] of sortedByFrequency) {
+            if(filteredByFrequency.length < this.MAX_RECEIPE_COUNT) {
+                filteredByFrequency.push({"receipeCode" : key, "info" : value});
+            }			
+
+        }
+
+        // 재료수 기준으로 정렬
+        filteredByFrequency.sort((a,b) => (a.info.receipeCounts > b.info.receipeCounts) ? 1 : ((a.info.receipeCounts < b.info.receipeCounts) ? -1 : 0)); 
+
+
+        let filteredByCounts = [];
+        for(let [key,value] of filteredByFrequency) {
 	   
             if(filteredByCounts.length < this.MAX_RECEIPE_COUNT) {
                 filteredByCounts.push(key);
             }			
 
         }
-	console.log(filteredByCounts);
+	    console.log(filteredByCounts);
         return filteredByCounts;
     }
 
     
-
 
     async findReceipe() {
 
@@ -97,26 +133,26 @@ const db = require('../db/db');
 	
 
         
-	this.receipes = filteredReceipe;
+	    this.receipes = filteredReceipe;
       	
 	    
     }
 
     getReceipes() {
 
-	let temp = [] ;
-	for(let [key,value] of this.receipes) {
-	   if(temp.length < 3) {
-		temp.push(value);
-		this.removeReceipe(key);
-	   }
-	}
-	return temp;
+        let temp = [] ;
+        for(let [key,value] of this.receipes) {
+        if(temp.length < 3) {
+            temp.push(value);
+            this.removeReceipe(key);
+        }
+        }
+        return temp;
 
     }
 
     removeReceipe(key) {
-	this.receipes.delete(key);
+    	this.receipes.delete(key);
     }
 
 
